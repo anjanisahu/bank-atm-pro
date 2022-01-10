6182817034a859d01d1fbb589503b2648bb6a21b
Anjani sahu <79141664+anjanisahu@users.noreply.github.com>
2022-01-10 15:12:02 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,14 @@
+
+
+class Person:
+
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+
+        self.address = address
+        self.telephone = telephone
+        self.email = email
+
+    
